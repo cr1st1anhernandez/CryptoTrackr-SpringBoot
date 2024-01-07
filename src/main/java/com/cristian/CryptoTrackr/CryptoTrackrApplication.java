c1d8a6a5ab11e296345292dfebf1435e6a019a7b
cristian <cristian0402218@gmail.com>
2024-01-06 18:58:32 -0600

    :tada: Initial commit

---
+++ src/main/java/com/cristian/CryptoTrackr/CryptoTrackrApplication.java
@@ -0,0 +1,13 @@
+package com.cristian.CryptoTrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristian/CryptoTrackr/CryptoTrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.CryptoTrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
