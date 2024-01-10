77f0bad770ec840651d91048c98caec82d08dc0a
cristian <cristian0402218@gmail.com>
2024-01-10 11:14:58 -0600

    :wrench: Add CORS configuration

---
+++ src/main/java/com/cristian/CryptoTrackr/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.cristian.CryptoTrackr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("https:crypto-trackr.vercel.app")
+        .allowedMethods("GET");
+  }
+}
